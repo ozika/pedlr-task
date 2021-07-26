9a0dfeffede5e084078261ca7175e916b1c0cfe9
Christoph Koch <koch@mpib-berlin.mpg.de>
2021-07-26 10:42:26 +0200

    Integrated arc functions into index

--- client/public/arc_seddataNend.js
+++
@@ -1,9 +0,0 @@
-// Source postData function
-src="arc_postData.js"
-
-// Function to send data and end experiment (will be called on finish)
-function senddataNend(){
-    var JSDATA = jsPsych.data.get().json()
-    const body = {partial: false, data: JSDATA} //, structure according to ARC
-    postData(JSON.stringify(body))
-}
--- client/public/arc_wait.js
+++
@@ -1,6 +0,0 @@
-// Function to wait on arc server response
-function wait(ms){
-    var start = new Date().getTime();
-    var end = start;
-    while(end < start + ms) {end = new Date().getTime(); }
- }
